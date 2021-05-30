d9fc0a90d1a4f8b95f6e18c7c0e367fea5534e5f
Lara Isak <luckasti.larry@gmail.com>
2021-05-30 23:41:47 +0200

    :construction: playing with game logic functions

--- script.js
+++ script.js
@@ -1,38 +1,43 @@
 const choices = ["coconut", "bible", "g&t"];
 const randomNum = Math.floor(Math.random() * choices.length);
-const hotPriestChoice = hotPriestPlay();
 const fleabagChoice = "g&t";
-const hotPriestScore = 0;
-const fleabagScore = 0;
+let hotPriestChoice;
+let hotPriestScore = 0;
+let fleabagScore = 0;
 
 function hotPriestPlay() {
+  for(let i = 0; i < 5; i++) {
+    
+  }
   return choices[randomNum];
 }
 
-function playRound(fleabagChoice, hotPriestChoice) {
+function playRound() {
+  hotPriestChoice = hotPriestPlay();
+  console.log(hotPriestChoice);
   if(fleabagChoice === hotPriestChoice) {
-    return "It's a tie!"
+    return "It's a tie!";
   }
   else if(fleabagChoice !== hotPriestChoice && hotPriestChoice === "bible") {
-    return "You won! G&T spills all over bible"
+    fleabagScore++;
+    return "You won! G&T spills all over bible";
   }
   else if(fleabagChoice !== hotPriestChoice && hotPriestChoice === "coconut") {
-    return "You lose! Coconut smashes G&T can"
+    hotPriestScore++;
+    return "You lose! Coconut smashes G&T can";
   }
 }
 
-function gameOn(round) {
-  for(let i = 0; i < 5; i++) {
-    if(round === "You won! G&T spills all over bible") {
-      fleabagScore++;
-      return "Fleabag is in the lead!";
-    }
-  }
-}
 
-console.log(fleabagChoice);
 console.log(hotPriestChoice);
 
 console.log(playRound(fleabagChoice, hotPriestChoice));
 
-console.log(gameOn(playRound(fleabagChoice, hotPriestChoice)));
+function gameOn() {
+  let display = playRound(fleabagChoice, hotPriestChoice);
+  document.querySelector("#test").innerHTML = display;
+}
+
+console.log(gameOn());
+
+
