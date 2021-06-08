8c8b31f0ead39fa62e25b9e60e0e3ef1b7d39c8e
Lara Isak <luckasti.larry@gmail.com>
2021-06-08 14:06:03 +0200

    ♻️ apply final, minor changes to the project

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const choices = ["coconut", "bible", "g&t"];
-let hotPriestScore = 0;
+let priestScore = 0;
 let fleabagScore = 0;
-const selectButtons = document.querySelectorAll('.gameButton');
-const displayText = document.querySelector('#gameText');
+const selectGameButtons = document.querySelectorAll('.gameButton');
+const displayGameText = document.querySelector('#gameText');
 const displayFleabagScore = document.querySelector('.fleabagScore');
-const displayHotPriestScore = document.querySelector('.hotPriestScore');
+const displayPriestScore = document.querySelector('.priestScore');
 let displayFleabagChoice = document.querySelector('.fleabagChoice');
-let displayHotPriestChoice = document.querySelector('.priestChoice');
+let displayPriestChoice = document.querySelector('.priestChoice');
 const fleabagSpiritAnimal = document.querySelector('.fleabagSpiritAnimal');
 const priestSpiritAnimal = document.querySelector('.priestSpiritAnimal');
 
 displayFleabagScore.textContent = fleabagScore;
-displayHotPriestScore.textContent = hotPriestScore;
+displayPriestScore.textContent = priestScore;
 
-function hotPriestPlay() {
+function priestPlay() {
   const randomNum = Math.floor(Math.random() * choices.length);
   return choices[randomNum];
 }
@@ -22,41 +22,41 @@ function playRound(val) {
   const fleabagChoice = val;
   displayFleabagChoice.textContent = fleabagChoice;
 
-  const hotPriestChoice = hotPriestPlay();
-  displayHotPriestChoice.textContent = hotPriestChoice;
+  const priestChoice = priestPlay();
+  displayPriestChoice.textContent = priestChoice;
 
-  if(fleabagChoice === "g&t" && hotPriestChoice === "bible") {
+  if(fleabagChoice === "g&t" && priestChoice === "bible") {
     fleabagScore++;
     displayFleabagScore.textContent = fleabagScore;
-    displayText.textContent = "Yesss! G&T spills all over Bible";
+    displayGameText.textContent = "You win! G&T spills all over Bible";
   }
-  else if(fleabagChoice === "g&t" && hotPriestChoice === "coconut") {
-    hotPriestScore++;
-    displayHotPriestScore.textContent = hotPriestScore;
-    displayText.textContent = "Shit! Coconut smashes G&T can";
+  else if(fleabagChoice === "g&t" && priestChoice === "coconut") {
+    priestScore++;
+    displayPriestScore.textContent = priestScore;
+    displayGameText.textContent = "You lose! Coconut smashes G&T can";
   }
-  else if(fleabagChoice === "bible" && hotPriestChoice === "coconut") {
+  else if(fleabagChoice === "bible" && priestChoice === "coconut") {
     fleabagScore++;
     displayFleabagScore.textContent = fleabagScore;
-    displayText.textContent = "Yesss! Bible covers Coconut";
+    displayGameText.textContent = "You win! Bible covers Coconut";
   }
-  else if(fleabagChoice === "bible" && hotPriestChoice === "g&t") {
-    hotPriestScore++;
-    displayHotPriestScore.textContent = hotPriestScore;
-    displayText.textContent = "Shit! G&T spills all over Bible";
+  else if(fleabagChoice === "bible" && priestChoice === "g&t") {
+    priestScore++;
+    displayPriestScore.textContent = priestScore;
+    displayGameText.textContent = "You lose! G&T spills all over Bible";
   }
-  else if(fleabagChoice === "coconut" && hotPriestChoice === "g&t") {
+  else if(fleabagChoice === "coconut" && priestChoice === "g&t") {
     fleabagScore++;
     displayFleabagScore.textContent = fleabagScore;
-    displayText.textContent = "Yesss! Coconut smashes G&T can";
+    displayGameText.textContent = "You win! Coconut smashes G&T can";
   }
-  else if(fleabagChoice === "coconut" && hotPriestChoice === "bible") {
-    hotPriestScore++;
-    displayHotPriestScore.textContent = hotPriestScore;
-    displayText.textContent = "Shit! Bible covers Coconut";
+  else if(fleabagChoice === "coconut" && priestChoice === "bible") {
+    priestScore++;
+    displayPriestScore.textContent = priestScore;
+    displayGameText.textContent = "You lose! Bible covers Coconut";
   }
   else {
-    displayText.textContent = "It's a tie!";
+    displayGameText.textContent = "It's a tie!";
   }
 
   gameOver();
@@ -64,36 +64,36 @@ function playRound(val) {
 
 function gameOver() {
   if(fleabagScore === 5) {
-    displayText.textContent = "Game Over! You won!";
+    displayGameText.textContent = "You won! Celebrate by feeding Hilary!";
     fleabagSpiritAnimal.style.transform = "translateY(-100px)";
     toggleButtonsAbility();
   }
-  else if(hotPriestScore === 5) {
-    displayText.textContent = "Game Over! You lost!";
+  else if(priestScore === 5) {
+    displayGameText.textContent = "You lost! Point the priest to the fox!";
     priestSpiritAnimal.style.transform = "translateY(-100px)";
     toggleButtonsAbility();
   }
 }
 
 function toggleButtonsAbility() {
-  for(let i = 0; i < selectButtons.length; i++) {
-    if(selectButtons[i].disabled === false) {
-      selectButtons[i].disabled = true;
+  for(let i = 0; i < selectGameButtons.length; i++) {
+    if(selectGameButtons[i].disabled === false) {
+      selectGameButtons[i].disabled = true;
     }
     else {
-      selectButtons[i].disabled = false;
+      selectGameButtons[i].disabled = false;
     }
   }
 }
 
 function gameReset() {
   fleabagScore = 0;
-  hotPriestScore = 0;
+  priestScore = 0;
   displayFleabagScore.textContent = fleabagScore;
-  displayHotPriestScore.textContent = hotPriestScore;
-  displayText.textContent = "";
+  displayPriestScore.textContent = priestScore;
+  displayGameText.textContent = "";
   displayFleabagChoice.textContent = "";
-  displayHotPriestChoice.textContent = "";
+  displayPriestChoice.textContent = "";
   fleabagSpiritAnimal.style.transform = "translateY(0px)";
   priestSpiritAnimal.style.transform = "translateY(0px)";
   toggleButtonsAbility();
